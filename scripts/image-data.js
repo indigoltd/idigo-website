16f11b846fa73de6100416aa1fe1742c81ebce51
indigoltd <dilpreetpannu1997@gmail.com>
2025-06-23 16:03:21 -0600

    Create image-data.js

---
+++ scripts/image-data.js
@@ -0,0 +1,27 @@
+// scripts/image-data.js
+const jobImages = [
+  {
+    src: "images/jobs/drywall-job1.jpg",
+    alt: "Drywall installation in custom home"
+  },
+  {
+    src: "images/jobs/insulation-job1.jpg",
+    alt: "Attic insulation"
+  },
+  {
+    src: "images/jobs/poly-install.jpg",
+    alt: "Poly vapor barrier installed"
+  },
+  {
+    src: "images/jobs/blowin-job1.jpg",
+    alt: "Blow-in insulation process"
+  },
+  {
+    src: "images/jobs/commercial-job.jpg",
+    alt: "Commercial drywall site"
+  },
+  {
+    src: "images/jobs/demo-after.jpg",
+    alt: "Clean post-demo space"
+  }
+];
